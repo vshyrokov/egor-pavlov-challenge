5c7665d7f8e84c6104036bfecb8ea1b491feed87
Viktor Shyrokov <viktor.shyrokov@toptal.com>
2022-05-27 09:59:19 +0200

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import "./styles.css";
+
+// api - https://swapi.dev/
+// 1. customers want to have a list of the characters
+// ...?
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
